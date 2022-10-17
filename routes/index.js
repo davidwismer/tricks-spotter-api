59e9b02069ca8d4fd9a1e12fe542d9c91add5f00
David Wismer <david.wismer@heig-vd.ch>
2022-10-17 17:39:49 +0200

    Render and MongoDB links

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Ignition!");
+  res.send("Bienvenue sur l'api!");
 });
 
 export default router;
