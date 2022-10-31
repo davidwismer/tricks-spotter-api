9a5e70e1db377335cc7d5548985624c22cc35140
Anas Ismaili <anas.ismaili@heig-vd.ch>
2022-10-31 14:49:48 +0100

    modeles et routes tricks

--- model/Trick.js
+++ model/Trick.js
@@ -1,8 +1,31 @@
 import mongoose, { Schema, model } from "mongoose";
+import uniqueValidator from 'mongoose-unique-validator';
 
-//create the user schema
+//create the trick schema
 let trickSchema = new Schema({
-    
+    id: {
+        type: mongoose.ObjectId
+    },
+    name: {
+        type: String,
+        required: [true, 'You must provide a name!']
+    },
+    creationDate: {
+        type: Date,
+        default: Date.now
+    },
+    LastModificationDate: {
+        type: Date,
+        default: Date.now
+    },
+    spotid: {
+        type: String,
+        required: [true, 'You must provide a name!']
+    },
+    userid: {
+        type: String,
+        required: [true, 'You must provide a name!']
+    },
 })
 
 //create model and export it
--- routes/tricks.js
+++ routes/tricks.js
@@ -1,8 +1,41 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { Trick } from "../model/Trick.js";
 const router = express.Router();
 
+//////////////////////////////////////////GET
+//get all tricks
 router.get("/", function (req, res, next) {
-  res.send("Page des tricks");
+  Trick.find().sort('name').exec(function (err, tricks) {
+    if (err) {
+      return next(err);
+    }
+    res.send(tricks);
+  });
 });
+
+//Get trick by id
+router.get("/:id", function (req, res, next) {
+  Trick.findOne({ _id: req.params.id }).exec(function (err, trick) {
+    if (err) {
+      return next(err)
+    }
+    res.send(trick)
+  })
+});
+
+////////////////////////////////////////////POST
+//Create new trick
+router.post("/", function (req, res, next) {
+  //Get the trick created
+  const newTrick = new Trick(req.body)
+  //save new trick created
+  newTrick.save(function (err, savedTrick) {
+    if (err) {
+      return next(err)
+    }
+    res.send(savedTrick)
+  })
+})
 
 export default router;
