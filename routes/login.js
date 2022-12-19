c2539b284a3dd9bb47ed90b67eee490343c25fce
David Wismer <david.wismer@heig-vd.ch>
2022-12-19 15:52:49 +0100

    added user object to login response

--- routes/login.js
+++ routes/login.js
@@ -46,7 +46,7 @@ router.post("/", function (req, res, next) {
                 if (err) {
                     return next(err)
                 }
-                res.status(200).send({ token: token })
+                res.status(200).send({ token: token, user: user })
             })
         });
     })
